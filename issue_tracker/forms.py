6932d6fec418f820d13a1fbe99625a2d98f08b61
Anastassiya Prokudina <anakess88@gmail.com>
2023-03-01 14:01:14 +0600

    Реализовала CRUD для задачи
    на базе классовых представлений.

---
+++ issue_tracker/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from issue_tracker.models import Issue, Type, Status
+
+
+class IssueForm(forms.ModelForm):
+    class Meta:
+        model = Issue
+        type = forms.ModelChoiceField(queryset=Type.objects.all())
+        status = forms.ModelChoiceField(queryset=Status.objects.all())
+        fields = ["summary", "status", "type", "description"]
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get("summary")
+        if len(summary) < 2:
+            raise ValidationError("Заголовок должен быть длиннее двух символов")
+        return summary
