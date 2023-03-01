7cfcb1a1136d986587c89566cf84d02307962aa9
Anastassiya Prokudina <anakess88@gmail.com>
2023-03-01 13:27:34 +0600

    Зарегистрировала все модели в админ-панели.
    Создала админа и добавила через админ-панель типы и статусы задач, и 6 тестовых задач с разными статусами и типами.

---
+++ issue_tracker/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+
+from issue_tracker.models import Issue, Status, Type
+
+
+# Register your models here.
+class IssueAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "summary",
+        "description",
+        "status",
+        "type",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+        "is_deleted",
+    )
+    list_filter = (
+        "id",
+        "summary",
+        "description",
+        "status",
+        "type",
+        "created_at",
+        "deleted_at",
+    )
+    search_fields = ("id", "summary", "status", "type")
+    fields = ("summary", "description", "status", "type")
+    readonly_fields = ("id", "created_at", "updated_at", "deleted_at", "is_deleted")
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "title")
+    list_filter = ("id", "name", "title")
+    search_fields = ("id", "name", "title")
+    fields = ("name", "title")
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "title")
+    list_filter = ("id", "name", "title")
+    search_fields = ("id", "name", "title")
+    fields = ("name", "title")
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
