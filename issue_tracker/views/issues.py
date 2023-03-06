9a7f90a5eb6cf2ee71a39f629337affdb477bd30
Anastassiya Prokudina <anakess88@gmail.com>
2023-03-06 10:57:46 +0600

    Написала модельную форму для модели задачи
     и использовала её в представлениях создания
     и обновления.

--- issue_tracker/views/issues.py
+++ issue_tracker/views/issues.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.views.generic import TemplateView, FormView
 
 from issue_tracker.forms import IssueForm
 from issue_tracker.models import Issue
@@ -14,40 +15,43 @@ class IssueDetail(TemplateView):
         return context
 
 
-class IssueUpdateView(TemplateView):
+class IssueUpdateView(FormView):
     template_name = "issue_update.html"
+    form_class = IssueForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.issue = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["issue"] = get_object_or_404(Issue, pk=kwargs["pk"])
-        context["form"] = IssueForm(instance=context["issue"])
+        context['issue'] = self.issue
         return context
 
-    def post(self, request, *args, **kwargs):
-        issue = get_object_or_404(Issue, pk=kwargs["pk"])
-        form = IssueForm(request.POST, instance=issue)
-        if form.is_valid():
-            form.save()
-            return redirect("issue_detail", pk=issue.pk)
-        return render(
-            request, "issue_update.html", context={"form": form, "issue": issue}
-        )
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.issue
+        return kwargs
+
+    def form_valid(self, form):
+        self.issue = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('issue_detail', kwargs={'pk': self.issue.pk})
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Issue, pk=pk)
 
 
-class IssueCreateView(TemplateView):
+class IssueCreateView(FormView):
     template_name = "issue_create.html"
+    form_class = IssueForm
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["form"] = IssueForm()
-        return context
-
-    def post(self, request, *args, **kwargs):
-        form = IssueForm(request.POST)
-        if form.is_valid():
-            issue = form.save()
-            return redirect("issue_detail", pk=issue.pk)
-        return render(request, "issue_create.html", context={"form": form})
+    def form_valid(self, form):
+        issue = form.save()
+        return redirect("issue_detail", pk=issue.pk)
 
 
 class IssueDeleteView(TemplateView):
@@ -67,4 +71,3 @@ class IssueConfirmDeleteView(TemplateView):
         context["issue"] = get_object_or_404(Issue, pk=kwargs["pk"])
         context["issue"].delete()
         return redirect("index")
-
