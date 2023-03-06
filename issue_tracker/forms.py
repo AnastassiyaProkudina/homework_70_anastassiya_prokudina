15251c7703bea419f0f25712a74ada66b093e60c
Anastassiya Prokudina <anakess88@gmail.com>
2023-03-06 10:58:44 +0600

    Добавила два кастомных валидатора для поля summary
    для валидации минимального и максимального количества символов в поле

--- issue_tracker/forms.py
+++ issue_tracker/forms.py
@@ -1,21 +1,49 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.forms import CheckboxSelectMultiple
+from django.core.validators import BaseValidator
+from django.forms import Select
 
 from issue_tracker.models import Issue, Type, Status
 
 
+class CustomMaxLenValidator(BaseValidator):
+    def __init__(self, limit_value=30):
+        self.limit_value = limit_value
+        message = 'Максимальная длина заголовка задачи %(limit_value)s. Вы ввели %(show_value)s символов.'
+        super().__init__(limit_value=limit_value, message=message)
+
+    def compare(self, value, limit_value):
+        print(value)
+        print(limit_value)
+        return value > limit_value
+
+    def clean(self, value):
+        return len(value)
+
+
+class CustomMinLenValidator(BaseValidator):
+    def __init__(self, limit_value=2):
+        self.limit_value = limit_value
+        message = "Минимальная длина заголовка %(limit_value)s символов. Вы ввели %(show_value)s символов"
+        super().__init__(limit_value=limit_value, message=message)
+
+    def compare(self, value, limit_value):
+        return value < limit_value
+
+    def clean(self, value):
+        return len(value)
+
+
 class IssueForm(forms.ModelForm):
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), widget=CheckboxSelectMultiple)
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(),
+                                          widget=Select(),
+                                          label='Тип',
+                                          )
+    summary = forms.CharField(validators=(CustomMaxLenValidator(), CustomMinLenValidator()), label="Заголовок")
+
+    status = forms.ModelChoiceField(queryset=Status.objects.all(),
+                                    label="Статус")
 
     class Meta:
         model = Issue
-
-        status = forms.ModelChoiceField(queryset=Status.objects.all())
         fields = ["summary", "type", "status", "description"]
 
-    def clean_summary(self):
-        summary = self.cleaned_data.get("summary")
-        if len(summary) < 2:
-            raise ValidationError("Заголовок должен быть длиннее двух символов")
-        return summary
